ac0432a2d5144d990e9516b7a9352f19078b26d4
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-26 12:04:35 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React,{Component} from 'react'
-import Weather from './components/Weather.jsx'
+import React, { Component } from 'react'
+import Add from './components/Add'
+import './App.css'
 
-export default class App extends Component{
-	render(){
+export default class App extends Component {
+	render() {
 		return (
-			<div>	
-				<Weather/>
+			<div className="todo-container">
+				<div className="todo-wrap">
+					<Add/>
+					<ul className="todo-main">
+						<li>
+							<label>
+								<input type="checkbox"/>
+								<span>xxxxx</span>
+							</label>
+							<button className="btn btn-danger" style={{display:'none'}}>删除</button>
+						</li>
+						<li>
+							<label>
+								<input type="checkbox"/>
+								<span>yyyy</span>
+							</label>
+							<button className="btn btn-danger" style={{display:'none'}}>删除</button>
+						</li>
+					</ul>
+					<div className="todo-footer">
+						<label>
+							<input type="checkbox"/>
+						</label>
+						<span>
+							<span>已完成0</span> / 全部2
+						</span>
+						<button className="btn btn-danger">清除已完成任务</button>
+					</div>
+				</div>
 			</div>
 		)
 	}
--- src/module1.js
+++
@@ -1,6 +0,0 @@
-export default {
-	a:1,
-	b:2
-}
-
-export const a = 1
