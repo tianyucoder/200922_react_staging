a828814708dd9d8b06b462a19c2b91df8697e952
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-26 11:09:22 +0800

    更新

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App (){
-	return <h1>这是我第一个脚手架案例</h1>
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-//引入react核心库，当年我们通过script标签引入
-import React from 'react';
-//引入react-dom，当年我们通过script标签引入
-import ReactDOM from 'react-dom';
-//引入App根组件
-import App from './App';
+/* import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root')) */
 
-
+import module1,{a} from './module1'
+// console.log(module1.a)
+// console.log(module1.b)
+console.log(a)
