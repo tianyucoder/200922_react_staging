985e265728da96f96e2a2abf992b33369ac7c4ca
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-26 10:36:25 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
-//使用函数定义了一个App组件
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+	return <h1>这是我第一个脚手架案例</h1>
 }
 
 export default App;
