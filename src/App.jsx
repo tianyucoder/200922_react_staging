1933f8f6d68abe9a178b0bb13bff141ffe449a8e
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-26 14:27:19 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Add from './components/Add'
+import List from './components/List'
+import Footer from './components/Footer'
 import './App.css'
 
 export default class App extends Component {
@@ -8,31 +10,8 @@ export default class App extends Component {
 			<div className="todo-container">
 				<div className="todo-wrap">
 					<Add/>
-					<ul className="todo-main">
-						<li>
-							<label>
-								<input type="checkbox"/>
-								<span>xxxxx</span>
-							</label>
-							<button className="btn btn-danger" style={{display:'none'}}>删除</button>
-						</li>
-						<li>
-							<label>
-								<input type="checkbox"/>
-								<span>yyyy</span>
-							</label>
-							<button className="btn btn-danger" style={{display:'none'}}>删除</button>
-						</li>
-					</ul>
-					<div className="todo-footer">
-						<label>
-							<input type="checkbox"/>
-						</label>
-						<span>
-							<span>已完成0</span> / 全部2
-						</span>
-						<button className="btn btn-danger">清除已完成任务</button>
-					</div>
+					<List/>
+					<Footer/>
 				</div>
 			</div>
 		)
