8f61ee12b5302d4c9dca5662bbb842bd548af5df
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-26 11:18:23 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-/* import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App/>,document.getElementById('root')) */
+ReactDOM.render(<App/>,document.getElementById('root'))
 
-import module1,{a} from './module1'
+// import module1,{a} from './module1'
 // console.log(module1.a)
 // console.log(module1.b)
-console.log(a)
+// console.log(a)
