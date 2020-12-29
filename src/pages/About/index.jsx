42ef72bfa239ec1e798b8517bba021f59d6dbb62
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-29 15:13:17 +0800

    更新

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class About extends Component {
 	componentDidMount(){
-		console.log('About组件收到的props是:',this.props)
+		console.log('About组件收到的props是:',)
 	}
 	render() {
 		return <h3>我是About的内容</h3>
