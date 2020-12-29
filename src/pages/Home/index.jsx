70b1e42136c0f92d991386e7231f3dc294d8432d
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-29 16:06:41 +0800

    更新

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
+import {NavLink,Switch,Route} from 'react-router-dom'
+import HomeMessage from '../HomeMessage'
+import HomeNews from '../HomeNews'
 
 export default class Home extends Component {
 	render() {
-		return <h3>我是Home的内容</h3>
+		return (
+			<div>
+				<h2>Home组件内容</h2>
+				<div>
+					{/* 导航区 */}
+					<ul className="nav nav-tabs">
+						<li>
+							<NavLink activeClassName="navigation" className="list-group-item" to="/news">News</NavLink>
+						</li>
+						<li>
+							<NavLink activeClassName="navigation" className="list-group-item" to="/message">Message</NavLink>
+						</li>
+					</ul>
+					<Switch>
+						<Route path="/news" component={HomeNews}/>
+						<Route path="/message" component={HomeMessage}/>
+					</Switch>
+				</div>
+			</div>
+		)
 	}
 }
